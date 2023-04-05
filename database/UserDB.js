3d86307e45225187e2d1c212bbcdde80fccd560c
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2023-04-05 12:53:58 -0700

    route to send product from userDatabase

--- database/UserDB.js
+++ database/UserDB.js
@@ -6,6 +6,14 @@ module.exports.getAllProducts = async (username) => {
     const dbName = `LMDM_${username}`;
     const productDB = await switchDB(dbName, ProductSchemas);
     const productModel = await getDBModel(productDB, 'Product');
-    const product = await productModel.find();
+    const products = await productModel.find();
+    return products;
+}
+
+module.exports.getProduct = async (store, id) => {
+    const dbName = `LMDM_${store}`;
+    const productDB = await switchDB(dbName, ProductSchemas);
+    const productModel = await getDBModel(productDB, 'Product');
+    const product = await productModel.findById(id);
     return product;
 }
--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../../utils/catchAsync');
-const {getAllProducts} = require('../../database/UserDB');
+const {getAllProducts, getProduct} = require('../../database/UserDB');
 const {verifyKey} = require('../../database/KeysDB');
 //this is the api call to
 //localhost:3000/api/allProducts?store=[username]&key=[apiKey]\
@@ -10,15 +10,16 @@ const {verifyKey} = require('../../database/KeysDB');
 //this will be done in verifyKey middleware
 
 router.get('/allProducts', verifyKey, catchAsync(async(req, res) => {
-    const store = req.query.store;
-    const apiKey = req.query.key;
+    const {store} = req.query
+    const products = await getAllProducts(store);
+    return res.json(products);
+}));
 
-    if(store && apiKey){
-        const products = await getAllProducts(store);
-        // const jsonString = JSON.stringify(products);
-        return res.json(products);
-    }
-    res.send('error')
+router.get('/:id', verifyKey, catchAsync(async(req, res) => {
+    const id = req.params.id;
+    const {store} = req.query;
+    const product = await getProduct(store, id);
+    return res.json(product);
 }));
 
 module.exports = router;
