64792db95f3357b6a45dc297c865ad5a43611157
Liam McCabe <89570377+lmccabe267@users.noreply.github.com>
2023-04-05 18:39:29 -0700

    VerifyUser matches url/Accounts read /render newpg

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-const User = require('./models/User');
+// const {getUser} = require('./database/Accounts');
 
 module.exports.isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
@@ -8,3 +8,12 @@ module.exports.isLoggedIn = (req, res, next) => {
 	}
 	next();
 };
+
+module.exports.verifyUser = (req, res, next) => {
+	const {id} = req.params;
+	if(!id == req.user._id.toString()){
+		req.flash('error', 'User verification failed');
+		return res.redirect('/users/login');
+	}
+	next();
+}
--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/User');
-const {isLoggedIn} = require('../middleware');
+const {isLoggedIn, verifyUser} = require('../middleware');
+const {getUser} = require('../database/AccountsDB');
 const {getAllProducts} = require('../database/UserDB');
 
 //renders the dashboard populated with user '/:id' information pulled from database
-router.get('/:id/dashboard', isLoggedIn, catchAsync(async (req, res) => {
-    const user = await User.findById(req.params.id);
+router.get('/:id/dashboard', isLoggedIn, verifyUser, catchAsync(async (req, res) => {
+    const user = await getUser(req.params.id);
     const products = await getAllProducts(user.username);
     res.render('users/dashboard', {user, products});
+}));
+
+router.get('/:id/new', isLoggedIn, verifyUser, catchAsync(async (req, res) => {
+    res.render('users/newProduct');
 }));
 
 
